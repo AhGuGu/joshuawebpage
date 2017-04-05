6f66907ad7762a19a259f62790a215823b2307eb
David Gu <centrically@gmail.com>
2017-04-05 20:58:54 +0800

    Setup contact form

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'main#index'
   get 'about' => 'main#about'
   get 'contact' => 'main#contact'
+  resources :contacts, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
